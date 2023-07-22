58de9fd44021a0b822d1ee257db1e753ef95f039
Wanwisa Waddington <kaywanwisa4@googlemail.com>
2023-07-22 16:39:55 +0000

    Add javascript to header nav menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,20 @@
 /*
-Get access to the header nav list and hamburger icon
+Get access to the header nav list and hamburger 
  */
-const hamburgerIcon = document.querySelector(".hamburger-icon");
-const headerNavList = document.querySelector(".header-nav-list");
+const hamburger = document.querySelector(".hamburger");
+const headerNavLists = document.querySelector(".header-nav-lists");
 
-/* Add eventListener type click to header nav list and hamburger icon */
-
-hamburgerIcon.addEventListener("click", () => {
-  hamburgerIcon.classList.toggle("active");
-  headerNavList.classList.toggle("active");
+/* Add eventListener type click to header nav list and hamburger  */
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  headerNavLists.classList.toggle("active");
 });
+
+/* Add eventListener type click to header nav link to remove
+class active (to close nav menu by click on each link)*/
+document.querySelectorAll(".nav-link").forEach((n) =>
+  n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    headerNavLists.classList.remove("active");
+  })
+);
