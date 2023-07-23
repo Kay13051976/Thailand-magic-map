f7ef49889b47b0089cdc55ad714d70a9c0cbce99
Wanwisa Waddington <kaywanwisa4@googlemail.com>
2023-07-23 19:54:49 +0000

    Add javascript function to carousel container

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,3 +18,41 @@ document.querySelectorAll(".nav-link").forEach((n) =>
     headerNavLists.classList.remove("active");
   })
 );
+
+/*Get access to the carousel container */
+const containerImg = document.getElementById("container-img");
+const leftBtn = document.getElementById("left-btn");
+const rightBtn = document.getElementById("right-btn");
+
+const img = document.querySelectorAll("#imgs img");
+
+let idx = 0;
+let interval = setInterval(slide, 2000);
+
+function slide() {
+  idx++;
+  changeImage();
+}
+
+function changeImage() {
+  if (idx > img.length - 1) {
+    idx = 0;
+  } else if (idx < 0) {
+    idx = img.length - 1;
+  }
+  containerImage.style.transform = `translateX(${-idx * 550}px)`;
+}
+leftBtn.addEventListener("click", () => {
+  idx--;
+  changeImage();
+  resetInterval();
+});
+rightBtn.addEventListener("click", () => {
+  idx++;
+  changeImage();
+  resetInterval();
+});
+
+function resetInterval() {
+  clearInterval(interval);
+}
