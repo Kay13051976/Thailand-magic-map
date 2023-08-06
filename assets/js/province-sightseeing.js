df251da7ad3ca277196d4b94853a3a2336639a35
Wanwisa Waddington <kaywanwisa4@googlemail.com>
2023-08-06 15:05:18 +0000

    Fix province sightseeing data js file

--- assets/js/province-sightseeing.js
+++ assets/js/province-sightseeing.js
@@ -1,182 +1,176 @@
-
-
-const provinceSightseeing = [{
-  id: "chiangmai",
-  province: 'CHIANGMAI',
-  image: 'assets/images/chiangmai.png',
-  sightseeing1: {
-    name: 'KAYAN PEOPLE',
-    image: 'assets/images/three-tribe-lady-from-northern-of-thailand.jpg'
+const provinceSightseeing = [
+  {
+    id: "chiangmai",
+    province: "CHIANGMAI",
+    image: "assets/images/chiangmai.png",
+    sightseeing1: {
+      name: "KAYAN PEOPLE",
+      image: "assets/images/three-tribe-lady-from-northern-of-thailand.jpg",
+    },
+    sightseeing2: {
+      name: "KUM KAN TEMPLE",
+      image: "assets/images/temple-from-north-of-thailand.jpg",
+    },
+    sightseeing3: {
+      name: "PANG CHANG",
+      image: "assets/images/elephant-in-forest.jpg",
+    },
+    sightseeing4: {
+      name: "HMONG TRIBE",
+      image: "assets/images/tribe-from-north-thailand.jpg",
+    },
+    sightseeing5: {
+      name: "RICE FIELD",
+      image: "assets/images/ricefield-nan.jpg",
+    },
+    sightseeing6: {
+      name: "KAOWSOI",
+      image: "assets/images/kaowsoi-nan.jpg",
+    },
   },
-  sightseeing2: {
-    name: 'KUM KAN TEMPLE',
-    image: 'assets/images/temple-from-north-of-thailand.jpg'
+  {
+    id: "chiangrai",
+    province: "CHIANGRAI",
+    image: "assets/images/chiangrai.png",
+    sightseeing1: {
+      name: "MONK",
+      image: "assets/images/monk.jpg",
+    },
+    sightseeing2: {
+      name: "CHIANGRAI ROAD",
+      image: "assets/images/nan-road.jpg",
+    },
+    sightseeing3: {
+      name: "PANG CHANG",
+      image: "assets/images/elephant-in-forest.jpg",
+    },
+    sightseeing4: {
+      name: "NAN ELEPHANT",
+      image: "assets/images/kanchanaburi-elephant.jpg",
+    },
+    sightseeing5: {
+      name: "WAT GIANT",
+      image: "assets/images/wat-giant.jpg",
+    },
+    sightseeing6: {
+      name: "TOM YAM KUNG",
+      image: "assets/images/tom-yam-kung.jpg",
+    },
   },
-  sightseeing3: {
-    name: 'PANG CHANG',
-    image: 'assets/images/elephant-in-forest.jpg'
+  {
+    id: "phrae",
+    province: "PHRAE",
+    image: "assets/images/phrae.png",
+    sightseeing1: {
+      name: "FRESH CHILLI",
+      image: "assets/images/fresh-chilli.jpg",
+    },
+    sightseeing2: {
+      name: "BARBECUE",
+      image: "assets/images/barbecue-floating-market.jpg",
+    },
+    sightseeing3: {
+      name: "TEMPLE NORTH",
+      image: "assets/images/wat-arun.jpg",
+    },
+    sightseeing4: {
+      name: "SKYSCRAPER",
+      image: "assets/images/skyscraper-in-bangkok.jpg",
+    },
+    sightseeing5: {
+      name: "WAT GIANT",
+      image: "assets/images/wat-giant.jpg",
+    },
+    sightseeing6: {
+      name: "LOCAL FOOD",
+      image: "assets/images/phuket-food.jpg",
+    },
   },
-  sightseeing4: {
-    name: 'HMONG TRIBE',
-    image: 'assets/images/tribe-from-north-thailand.jpg'
+  {
+    id: "lampang",
+    province: "LAMPANG",
+    image: "assets/images/lampang.png",
+    sightseeing1: {
+      name: "CLAY POT",
+      image: "assets/images/nakhonratchasima-clay-pot.jpg",
+    },
+    sightseeing2: {
+      name: "OA BAY",
+      image: "assets/images/ao-nang-phiphi.jpg",
+    },
+    sightseeing3: {
+      name: "TUKTUK",
+      image: "assets/images/tuktuk-in-chaina-town-bangkok.jpg",
+    },
+    sightseeing4: {
+      name: "JAME BOND ISLAND",
+      image: "assets/images/phuket-jame-bond-island.jpg",
+    },
+    sightseeing5: {
+      name: "CAR PARK",
+      image: "assets/images/pattaya-car-park.jpg",
+    },
+    sightseeing6: {
+      name: "PADMEE",
+      image: "assets/images/padmee.korat.jpg",
+    },
   },
-  sightseeing5: {
-    name: 'RICE FIELD',
-    image: 'assets/images/ricefield-nan.jpg'
+  {
+    id: "ืnan",
+    province: "LAMPANG",
+    image: "assets/images/lampang.png",
+    sightseeing1: {
+      name: "OLD BEACH",
+      image: "assets/images/pattaya-bay.jpg",
+    },
+    sightseeing2: {
+      name: "WOODEN TEMPLE",
+      image: "assets/images/heaw-lam-pong.jpg",
+    },
+    sightseeing3: {
+      name: "PAT BEACH",
+      image: "assets/images/pattaya-beach.jpg",
+    },
+    sightseeing4: {
+      name: "SNORKELING",
+      image: "assets/images/phuket-snorkeling.jpg",
+    },
+    sightseeing5: {
+      name: "WHITE TEMPLE",
+      image: "assets/images/pattaya-temple.jpg",
+    },
+    sightseeing6: {
+      name: "GREEN CURRY",
+      image: "assets/images/green-curry.jpg",
+    },
   },
-  sightseeing6: {
-    name: 'KAOWSOI',
-    image: 'assets/images/kaowsoi-nan.jpg'
-  }
-},{
-id: "chiangrai",
-province: 'CHIANGRAI',
-image: 'assets/images/chiangrai.png',
-sightseeing1: {
-  name: 'MONK',
-  image: 'assets/images/monk.jpg'
-},
-sightseeing2: {
-  name: 'CHIANGRAI ROAD',
-  image: 'assets/images/nan-road.jpg'
-},
-sightseeing3: {
-  name: 'PANG CHANG',
-  image: 'assets/images/elephant-in-forest.jpg'
-},
-sightseeing4: {
-  name: 'NAN ELEPHANT',
-  image: 'assets/images/kanchanaburi-elephant.jpg'
-},
-sightseeing5: {
-  name: 'WAT GIANT',
-  image: 'assets/images/wat-giant.jpg'
-},
-sightseeing6: {
-  name: 'TOM YAM KUNG',
-  image: 'assets/images/tom-yam-kung.jpg'
-
-}
-},
-{
-  id: "phrae",
-province: 'PHRAE',
-image: 'assets/images/phrae.png',
-sightseeing1: {
-  name: 'FRESH CHILLI',
-  image: 'assets/images/fresh-chilli.jpg'
-},
-sightseeing2: {
-  name: 'BARBECUE',
-  image: 'assets/images/barbecue-floating-market.jpg'
-},
-sightseeing3: {
-  name: 'TEMPLE NORTH',
-  image: 'assets/images/wat-arun.jpg'
-},
-sightseeing4: {
-  name: 'SKYSCRAPER',
-  image: 'assets/images/skyscraper-in-bangkok.jpg'
-},
-sightseeing5: {
-  name: 'WAT GIANT',
-  image: 'assets/images/wat-giant.jpg'
-},
-sightseeing6: {
-  name: 'LOCAL FOOD',
-  image: 'assets/images/phuket-food.jpg'
-
-}
-},
-{
-  id: "lampang",
-province: 'LAMPANG',
-image: 'assets/images/lampang.png',
-sightseeing1: {
-  name: 'CLAY POT',
-  image: 'assets/images/nakhonratchasima-clay-pot.jpg'
-},
-sightseeing2: {
-  name: 'OA BAY',
-  image: 'assets/images/ao-nang-phiphi.jpg'
-},
-sightseeing3: {
-  name: 'TUKTUK',
-  image: 'assets/images/tuktuk-in-chaina-town-bangkok.jpg'
-},
-sightseeing4: {
-  name: 'JAME BOND ISLAND',
-  image: 'assets/images/phuket-jame-bond-island.jpg'
-},
-sightseeing5: {
-  name: 'CAR PARK',
-  image: 'assets/images/pattaya-car-park.jpg'
-},
-sightseeing6: {
-  name: 'PADMEE',
-  image: 'assets/images/padmee.korat.jpg'
-
-}
-},
-{
-  id: "lampang",
-province: 'LAMPANG',
-image: 'assets/images/lampang.png',
-sightseeing1: {
-  name: 'OLD BEACH',
-  image: 'assets/images/pattaya-bay.jpg'
-},
-sightseeing2: {
-  name: 'WOODEN TEMPLE',
-  image: 'assets/images/heaw-lam-pong.jpg'
-},
-sightseeing3: {
-  name: 'PAT BEACH',
-  image: 'assets/images/pattaya-beach.jpg'
-},
-sightseeing4: {
-  name: 'SNORKELING',
-  image: 'assets/images/phuket-snorkeling.jpg'
-},
-sightseeing5: {
-  name: 'WHITE TEMPLE',
-  image: 'assets/images/pattaya-temple.jpg'
-},
-sightseeing6: {
-  name: 'GREEN CURRY',
-  image: 'assets/images/green-curry.jpg'
-
-}
-},
-{
-  id: "maehongson",
-province: 'MAEHONGSON',
-image: 'assets/images/maehongson.png',
-sightseeing1: {
-  name: 'CHAOPHRAYA RIVER',
-  image: 'assets/images/chaophraya-river-bangkok.jpg'
-},
-sightseeing2: {
-  name: 'FARM LIFE',
-  image: 'assets/images/mountain-nan.jpg'
-},
-sightseeing3: {
-  name: 'FLOATING MARKET',
-  image: 'assets/images/floating-market-damnoen.jpg'
-},
-sightseeing4: {
-  name: 'SNORKELING',
-  image: 'assets/images/phuket-snorkeling.jpg'
-},
-sightseeing5: {
-  name: 'SNAKE TEMPLE',
-  image: 'assets/images/snake-temple.jpg'
-},
-sightseeing6: {
-  name: 'GREEN CURRY',
-  image: 'assets/images/green-curry.jpg'
-
-}
-}
-
+  {
+    id: "maehongson",
+    province: "MAEHONGSON",
+    image: "assets/images/maehongson.png",
+    sightseeing1: {
+      name: "CHAOPHRAYA RIVER",
+      image: "assets/images/chaophraya-river-bangkok.jpg",
+    },
+    sightseeing2: {
+      name: "FARM LIFE",
+      image: "assets/images/mountain-nan.jpg",
+    },
+    sightseeing3: {
+      name: "FLOATING MARKET",
+      image: "assets/images/floating-market-damnoen.jpg",
+    },
+    sightseeing4: {
+      name: "SNORKELING",
+      image: "assets/images/phuket-snorkeling.jpg",
+    },
+    sightseeing5: {
+      name: "SNAKE TEMPLE",
+      image: "assets/images/snake-temple.jpg",
+    },
+    sightseeing6: {
+      name: "GREEN CURRY",
+      image: "assets/images/green-curry.jpg",
+    },
+  },
 ];
