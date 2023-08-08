4b29039eefb9c28168d054a61676b07e4c91db87
Wanwisa Waddington <kaywanwisa4@googlemail.com>
2023-08-08 21:03:40 +0000

    Add js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,14 @@
 const buttonResetId = "btn-reset-map";
+const buttonBackId = "btn-back";
 const buttonMapClass = "btn-thailand-map";
+const buttonProvinceMapId = "part-province-page-main-col2";
+const partProvinceNorth = "part-province-northern";
+const buttonResetProvinceId = "btn-reset-province-map";
+const pagePartId = "part-province-page";
+const pageProvinceId = "province-sightseeing-page";
+const buttonSightSeeningClass = "province-sightseeing-page-main-col2";
+const buttonResetSightseeingId = "btn-reset-sightseeing";
+
 
 /*
 *Get access to the header nav list and hamburger 
@@ -16,10 +25,22 @@ const headerNavLists = document.querySelector(".header-nav-lists");
  */
 const animatedImage = document.querySelector(".main-thailand-map-hero-image");
 
-/*Get access to reset button Element on thailand map page */
+/*Get access to from thailand map page */
 const elmResetMapButton = document.getElementById(buttonResetId);
-/*Get access to map click button Element on thailand map page */
 const elmMapButton = document.getElementsByClassName(buttonMapClass);
+const elmBackButton = document.getElementById(buttonBackId);
+
+
+/*Get access to from Part page */
+const elmPartPage = document.getElementById(pagePartId);
+const elmProvinceMapButton = document.getElementsByClassName(buttonProvinceMapId);
+const elmResetProvinceMapButton = document.getElementById(buttonResetProvinceId);
+
+/*Get access to from SightSeeing page */
+const elmProvicePage = document.getElementById(pageProvinceId);
+const elmSightSeeningButton = document.getElementsByClassName(buttonSightSeeningClass);
+const elmResetSightseeingButton = document.getElementById(buttonResetSightseeingId);
+
 
 /* Hamburger slide in animation*/
 hamburger.addEventListener("click", () => {
@@ -34,8 +55,74 @@ document.querySelectorAll(".nav-link").forEach((n) =>
   })
 );
 
-/*Create function  event to work after download home page prevent flickering page*/
+function onClickProvince() {
+  var id = this.getAttribute("id");
+  window.location.href = "province-sightseeing.html?id=" + id;
+}
 
+function showHideElement(id) {
+  const img = document.getElementById(id);
+  if (img && img.style.opacity === "1") {
+    img.style.opacity = 0;
+  } else {
+    img.style.opacity = 1;
+  }
+}
+
+function onClickMap() {
+  const elementId = this.getAttribute("value");
+  if (elementId) {
+    showHideElement(elementId);
+  }
+}
+
+function bindResetButtonEventClick() {
+  for (let i = 0; i < elmMapButton.length; i++) {
+    const mapId = elmMapButton[i].getAttribute("value");
+    const img = document.getElementById(mapId);
+    if (img) {
+      img.style.opacity = 0;
+    }
+  }
+}
+
+function onClickPart() {
+  const elementId = this.querySelector('img').getAttribute('value');
+  if (elementId) {
+    const provinceLists = document.getElementById(elementId).getElementsByClassName('parentContainer')
+    for (let i = 0; i < provinceLists.length; i++) {
+      provinceLists[i].style.opacity = 1;
+    }
+  }
+}
+
+function bindResetProvinceButtonEventClick() {
+  const provinceLists = document.getElementsByClassName('parentContainer');
+  for (let i = 0; i < provinceLists.length; i++) {
+    provinceLists[i].style.opacity = 0;
+  }
+}
+
+
+function onClickSightSeeing() {
+  const elementId = this.querySelector('img').getAttribute('value');
+  if (elementId) {
+    const provinceLists = document.getElementById('province-section-' + elementId).getElementsByClassName('parentContainer')
+    for (let i = 0; i < provinceLists.length; i++) {
+      provinceLists[i].style.opacity = 1;
+    }
+  }
+}
+
+function bindResetSightseeingButtonEventClick() {
+  const sightseeingLists = document.getElementsByClassName('parentContainer');
+  for (let i = 0; i < sightseeingLists.length; i++) {
+    sightseeingLists[i].style.opacity = 0;
+  }
+}
+
+
+/*Create function  event to work after download home page prevent flickering page*/
 document.addEventListener("DOMContentLoaded", function (event) {
   const thailand_map = document.getElementById("thailand-map");
   if (thailand_map) {
@@ -79,116 +166,117 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
   // addEventListener click to reset button
   if (elmResetMapButton) {
-    elmResetMapButton.addEventListener(
-      "click",
-      bindResetButtonEventClick,
-      false
-    );
-  }
-});
-
-// Part province page
-// Get parameter from URL
-
-const queryString = window.location.search;
-const urlParams = new URLSearchParams(queryString);
-const id = urlParams.get("id");
-if (id) {
-  const province = document.getElementById("part-province-" + id);
-  if (province) {
-    province.style.display = "grid";
+    elmResetMapButton.addEventListener("click", bindResetButtonEventClick, false);
   }
 
-  const title = document.getElementById("part-province-name");
-  if (title) {
-    title.innerText = id.toUpperCase();
+  // addEventListener click to reset button
+  if (elmBackButton) {
+    elmBackButton.addEventListener("click", () => {
+      history.back();
+    }, false);
   }
-}
 
-const province = document.getElementsByClassName("onclick-province");
+  if (elmProvinceMapButton) {
+    for (var i = 0; i < elmProvinceMapButton.length; i++) {
+      elmProvinceMapButton[i].addEventListener("click", onClickPart, false);
+    }
+  }
 
-function onClickProvince() {
-  var id = this.getAttribute("id");
-  window.location.href = "province-sightseeing.html?id=" + id;
-}
-for (var i = 0; i < province.length; i++) {
-  province[i].addEventListener("click", onClickProvince, false);
-}
+  // addEventListener click to reset button
+  if (elmResetProvinceMapButton) {
+    elmResetProvinceMapButton.addEventListener("click", bindResetProvinceButtonEventClick, false);
+  }
 
-// province-sightseeing page
-if (id) {
-  if (
-    window.location.pathname == "/Thailand-magic-map/province-sightseeing.html"
-  ) {
-    var provinceObject = provinceSightseeing.find((x) => x.id === id);
+  if (elmPartPage) {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const partId = urlParams.get("id");
+    if (partId) {
+      const partName = document.getElementById("part-province-" + partId);
+      if (partName) {
+        partName.style.display = "grid";
+      }
 
-    if (provinceObject) {
-      var s1img = document.getElementById("sightseeing1-img");
-      var s2img = document.getElementById("sightseeing2-img");
-      var s3img = document.getElementById("sightseeing3-img");
-      var s4img = document.getElementById("sightseeing4-img");
-      var s5img = document.getElementById("sightseeing5-img");
-      var s6img = document.getElementById("sightseeing6-img");
+      const title = document.getElementById("part-province-name");
+      if (title) {
+        title.innerText = partId.toUpperCase();
+      }
 
-      var s1name = document.getElementById("sightseeing1-name");
-      var s2name = document.getElementById("sightseeing2-name");
-      var s3name = document.getElementById("sightseeing3-name");
-      var s4name = document.getElementById("sightseeing4-name");
-      var s5name = document.getElementById("sightseeing5-name");
-      var s6name = document.getElementById("sightseeing6-name");
+      const provinceLists = partName.getElementsByClassName("onclick-province");
+      for (var i = 0; i < provinceLists.length; i++) {
+        provinceLists[i].addEventListener("click", onClickProvince, false);
+      }
 
-      var provinceImage = document.getElementById("province-image");
-      provinceImage.src = provinceObject.image;
+    }
+  }
 
-      s1img.src = provinceObject.sightseeing1.image;
-      s2img.src = provinceObject.sightseeing2.image;
-      s3img.src = provinceObject.sightseeing3.image;
-      s4img.src = provinceObject.sightseeing4.image;
-      s5img.src = provinceObject.sightseeing5.image;
-      s6img.src = provinceObject.sightseeing6.image;
 
-      s1name.innerText = provinceObject.sightseeing1.name;
-      s2name.innerText = provinceObject.sightseeing2.name;
-      s3name.innerText = provinceObject.sightseeing3.name;
-      s4name.innerText = provinceObject.sightseeing4.name;
-      s5name.innerText = provinceObject.sightseeing5.name;
-      s6name.innerText = provinceObject.sightseeing6.name;
-
-      const province = document.getElementById("province-section");
+  if (elmProvicePage) {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const sightSeeningid = urlParams.get("id");
+    if (sightSeeningid) {
+      const province = document.getElementById("province-section-" + sightSeeningid);
       if (province) {
         province.style.display = "grid";
       }
+
+      const title = document.getElementById("province-name");
+      if (title) {
+        title.innerText = sightSeeningid.toUpperCase();
+      }
+
+      var provinceObject = provinceSightseeing.find((x) => x.id === sightSeeningid);
+
+      if (provinceObject) {
+        var s1img = document.getElementById("sightseeing1-img");
+        var s2img = document.getElementById("sightseeing2-img");
+        var s3img = document.getElementById("sightseeing3-img");
+        var s4img = document.getElementById("sightseeing4-img");
+        var s5img = document.getElementById("sightseeing5-img");
+        var s6img = document.getElementById("sightseeing6-img");
+
+        var s1name = document.getElementById("sightseeing1-name");
+        var s2name = document.getElementById("sightseeing2-name");
+        var s3name = document.getElementById("sightseeing3-name");
+        var s4name = document.getElementById("sightseeing4-name");
+        var s5name = document.getElementById("sightseeing5-name");
+        var s6name = document.getElementById("sightseeing6-name");
+
+        var provinceImage = document.getElementById("province-image");
+        provinceImage.src = provinceObject.image;
+
+        s1img.src = provinceObject.sightseeing1.image;
+        s2img.src = provinceObject.sightseeing2.image;
+        s3img.src = provinceObject.sightseeing3.image;
+        s4img.src = provinceObject.sightseeing4.image;
+        s5img.src = provinceObject.sightseeing5.image;
+        s6img.src = provinceObject.sightseeing6.image;
+
+        s1name.innerText = provinceObject.sightseeing1.name;
+        s2name.innerText = provinceObject.sightseeing2.name;
+        s3name.innerText = provinceObject.sightseeing3.name;
+        s4name.innerText = provinceObject.sightseeing4.name;
+        s5name.innerText = provinceObject.sightseeing5.name;
+        s6name.innerText = provinceObject.sightseeing6.name;
+      }
+
+      const sightSeeginigLists = province.getElementsByClassName("parentContainer");
+      for (var i = 0; i < sightSeeginigLists.length; i++) {
+        sightSeeginigLists[i].addEventListener("click", onClickSightSeeing, false);
+      }
+
     }
+  }
 
-    const title = document.getElementById("province-name");
-    if (title) {
-      title.innerText = id.toUpperCase();
+  if (elmSightSeeningButton) {
+    for (var i = 0; i < elmSightSeeningButton.length; i++) {
+      elmSightSeeningButton[i].addEventListener("click", onClickSightSeeing, false);
     }
   }
-}
 
-function showHideElement(id) {
-  const img = document.getElementById(id);
-  if (img && img.style.opacity === "1") {
-    img.style.opacity = 0;
-  } else {
-    img.style.opacity = 1;
+  if (elmResetSightseeingButton) {
+    elmResetSightseeingButton.addEventListener("click", bindResetSightseeingButtonEventClick, false);
   }
-}
 
-function onClickMap() {
-  const elementId = this.getAttribute("value");
-  if (elementId) {
-    showHideElement(elementId);
-  }
-}
-
-function bindResetButtonEventClick() {
-  for (let i = 0; i < elmMapButton.length; i++) {
-    const mapId = elmMapButton[i].getAttribute("value");
-    const img = document.getElementById(mapId);
-    if (img) {
-      img.style.opacity = 0;
-    }
-  }
-}
+});
